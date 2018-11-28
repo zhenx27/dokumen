4caf64c95db02232cd0e8b90a94297ee01c78cd9
zhenx27 <43463999+zhenx27@users.noreply.github.com>
2018-11-28 18:46:17 +0700

    Bot toto news
    Toto news

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBh3eWu9ONsMDOe8uxeo6xfZyylYKpipnAdXaahaOw0IudotCvmIqlv9tzzq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "5adc7247770b53e3";
+$os="android";
+$sign="893f643fd9a493fc0c1c89f281f33e818a48f11c610c054bbae0ed9860a60234";
